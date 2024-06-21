2bc35ae2b679955addea349274af7becabc76b97
markusedgar <markus@workplayexperience.com>
2024-06-22 00:35:23 +0200

    Update main.py

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -26,8 +26,8 @@ MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
 # Uncomment to enable debug mode
 # MODEL_LIST.insert(0, "debug")
 
-st.logo("/mount/src/experiment_knowledge_gpt/knowledge_gpt/logo.png", "/mount/src/experiment_knowledge_gpt/knowledge_gpt/logo.png")
 st.set_page_config(page_title="KnowledgeGPT", page_icon="📖", layout="wide")
+st.header("📖KnowledgeGPT")
 
 # Enable caching for expensive functions
 bootstrap_caching()
